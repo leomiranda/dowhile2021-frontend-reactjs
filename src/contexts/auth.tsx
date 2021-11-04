252550c2c4fcbedf801d1828b275f33332aea62d
Leo Miranda <contato@leomiranda.com>
2021-11-04 08:53:38 -0400

    feat(message): send & io refresh

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -44,6 +44,8 @@ export function AuthProvider(props: AuthProviderInterface) {
 
 		localStorage.setItem('@dowhile:token', token);
 
+		api.defaults.headers.common.authorization = `Bearer ${token}`;
+
 		setUser(user);
 	}
 
